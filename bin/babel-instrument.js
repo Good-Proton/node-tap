1329e741eb922e33d2832aaf1637c98967fea786
Vyacheslav Dorzhiev <aliencrowdman@yandex.ru>
2020-07-27 08:20:19 +0300

    dump `__coverage__` for `electron`

--- bin/babel-instrument.js
+++ bin/babel-instrument.js
@@ -1,3 +1,10 @@
 require('@babel/register')({
     plugins: ['istanbul']
 });
+
+const fs = require('fs');
+require('electron').app.on('quit', () => {
+    if (global.__coverage__) {
+        fs.writeFileSync(`.nyc_output/${Date.now()}-${process.pid}.json`, JSON.stringify(global.__coverage__));
+    }
+})
